bb15dddf2fc8eb1403476a20bf1b13e9ecc1133e
Jane <37192026+jane88nausicaa@users.noreply.github.com>
2018-04-24 09:42:02 -0700

    Add files via upload

---
+++ config/dev.js
@@ -0,0 +1,12 @@
+module.exports = {
+  googleClientID:
+    "777796469111-pgoe7pec42bfn7pk7rasumsbu780heir.apps.googleusercontent.com",
+  googleClientSecret: "Qj1Slnj8ePxsPeHJI-de64uW",
+  mongoURI: "mongodb://jane:password@ds253889.mlab.com:53889/emaily-deva",
+  cookieKey: "uioebmsg7rklw5fl892bgkse104suqcdlk",
+  stripePublishableKey: "pk_test_hL8wJEMEJ0YnIoPl6lMOutPq",
+  stripeSecretKey: "sk_test_TNIQOTni3b30WCSgTeEbLHyR",
+  sendGridKey:
+    "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",
+  redirectDomain: "http://localhost:3000"
+};
